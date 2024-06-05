5c1458ee8a0f8fbd027e389874f4840765f9109e
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-06-05 16:44:29 +0400

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Bakar Kharabadze on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
